0462f0d65094d2e850aa51442c03c1f75107e03c
Patrick Obermueller <patrick.obermueller@willhaben.at>
2021-10-23 19:50:48 +0200

    add pig draggable to control panel page

--- applications/frontend/src/app/components/overlay/game/objects/Pig.ts
+++ applications/frontend/src/app/components/overlay/game/objects/Pig.ts
@@ -12,5 +12,16 @@ export class Pig extends Phaser.GameObjects.Image {
 		this.setName('pig')
 		this.setScale(0.5)
 		scene.add.existing(this)
+
+		this.setInteractive()
+		scene.input.setDraggable(this)
+		this.on('pointerout', () => {
+			console.log('Pig position updated.')
+		})
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		scene.input.on('drag', (_pointer: any, _gameObject: any, dragX: any, dragY: any) => {
+			this.x = dragX
+			this.y = dragY
+		})
 	}
 }
