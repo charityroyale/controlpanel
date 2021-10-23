fe0ca792667169c3d33abd6b88a275a6bc836473
Patrick Obermueller <patrick.obermueller@willhaben.at>
2021-10-23 19:25:50 +0200

    make iframe responsive

--- applications/frontend/src/app/components/controlpanel/CenterPanel.tsx
+++ applications/frontend/src/app/components/controlpanel/CenterPanel.tsx
@@ -1,10 +1,39 @@
 /* eslint-disable jsx-a11y/accessible-emoji */
-import React from 'react'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
 import { FunctionComponent } from 'react'
 import { Label, Content } from '../../../pages/controlpanel'
-import { styled } from '../../styles/Theme'
+import { responsiveMaxSizeThreshold, styled } from '../../styles/Theme'
 
 export const CenterPanel: FunctionComponent = () => {
+	const contentRef = useRef<null | HTMLDivElement>(null)
+	const [scale, setScale] = useState(0)
+
+	const updateIframeSize = useCallback(() => {
+		if (contentRef.current) {
+			const { offsetHeight } = contentRef.current
+			console.log(offsetHeight)
+
+			let newHeightScale
+			if (window.screen.width <= responsiveMaxSizeThreshold.phone) {
+				// Mobile iFrame hack
+				newHeightScale = (offsetHeight - 8) / 1080
+			} else {
+				newHeightScale = offsetHeight / 1080
+			}
+
+			setScale(Number(newHeightScale.toFixed(10)))
+		}
+	}, [])
+
+	useEffect(() => {
+		updateIframeSize()
+	}, [updateIframeSize])
+
+	useEffect(() => {
+		window.addEventListener('resize', updateIframeSize)
+		return () => window.removeEventListener('resize', updateIframeSize)
+	}, [updateIframeSize])
+
 	return (
 		<GridCenterPanel>
 			<Label>
@@ -13,12 +42,19 @@ export const CenterPanel: FunctionComponent = () => {
 				</LiveEmoji>
 				Pigview
 			</Label>
-			<Content>
-				<iframe title="overlay" src="/overlay"></iframe>
+			<Content ref={contentRef} style={{ padding: 0 }}>
+				<OverlayIframe title="overlay" src="/overlay" height={1080} width={1920} scale={scale}></OverlayIframe>
 			</Content>
 		</GridCenterPanel>
 	)
 }
+
+const OverlayIframe = styled.iframe<{ scale: number }>`
+	border: none;
+	display: block;
+	transform: scale(${(p) => p.scale});
+	transform-origin: 0 0;
+`
 
 const LiveEmoji = styled.span`
 	font-size: ${(p) => p.theme.fontSize.s}px;
