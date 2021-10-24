20c015cdeba6348b6a65e6acb6d2fb220372c4dc
Patrick Obermueller <patrick.obermueller@willhaben.at>
2021-10-24 20:03:54 +0200

    add donationslist and donation trigger listener

--- applications/frontend/src/app/components/controlpanel/RightPanel/RightPanel.tsx
+++ applications/frontend/src/app/components/controlpanel/RightPanel/RightPanel.tsx
@@ -1,51 +1,62 @@
-import React from 'react'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
 import { FunctionComponent } from 'react'
 import { Label, Content } from '../../../../pages/controlpanel'
 import { styled } from '../../../styles/Theme'
 import { GrMoney } from 'react-icons/gr'
-import { Donation } from '@pftp/common'
+import { Donation, DONATION_TRIGGER } from '@pftp/common'
 import { Donations } from './DonationList'
+import { useSocket } from '../../../hooks/useSocket'
 
 export const RightPanel: FunctionComponent = () => {
-	const donations: Donation[] = [
-		{
-			user: 'heideltrautEUW',
-			amount: 50.2,
-			timestamp: new Date().toDateString(),
-		},
-		{
-			user: 'heideltrautEUW',
-			amount: 50.2,
-			timestamp: new Date().toDateString(),
-		},
-		{
-			user: 'heideltrautEUW',
-			amount: 50.2,
-			timestamp: new Date().toDateString(),
-		},
-		{
-			user: 'heideltrautEUW',
-			amount: 50.2,
-			timestamp: new Date().toDateString(),
-		},
-		{
-			user: 'heideltrautEUW',
-			amount: 50.2,
-			timestamp: new Date().toDateString(),
-		},
-		{
-			user: 'heideltrautEUW',
-			amount: 50.2,
-			timestamp: new Date().toDateString(),
-		},
-		{
-			user: 'heideltrautEUW',
-			amount: 50.2,
-			timestamp: new Date().toDateString(),
-		},
-	]
+	const { socket } = useSocket()
+	const [donations, setDonations] = useState<Donation[]>([])
+
+	const donationContentRef = useRef<HTMLDivElement | null>(null)
+	const donationContentInnerRef = useRef<HTMLDivElement | null>(null)
+
+	const [showScrollTop, setShowScrollTop] = useState(false)
+	const [showScrollBottom, setShowScrollBottom] = useState(false)
+
+	useEffect(() => {
+		socket?.on(DONATION_TRIGGER, (donation: Donation) => {
+			if (donationContentInnerRef.current && donationContentRef.current) {
+				const donationContentInnerClientHeight = donationContentInnerRef.current.clientHeight
+				const donationContentClientHeight = donationContentRef.current.clientHeight
+				if (donationContentInnerClientHeight + 50 > donationContentClientHeight) {
+					setShowScrollBottom(true)
+				}
+			}
+			const donationsCopy = donations
+			donationsCopy.unshift(donation)
+			setDonations([...donationsCopy])
+		})
+		/**
+		 * Unfortunately there is no better implementation YET with the missing donations dependency
+		 * as it would re-add the socketIO listener exponentially. This is dangerous nevertheless,
+		 * as state-changes might get lost.
+		 */
+	}, [socket, setDonations, setShowScrollBottom])
+
+	const onScroll = useCallback(() => {
+		if (donationContentRef.current) {
+			const { scrollTop, scrollHeight, clientHeight } = donationContentRef.current
+			if (scrollTop + clientHeight === scrollHeight) {
+				setShowScrollBottom(false)
+			} else {
+				setShowScrollBottom(true)
+			}
+
+			if (scrollTop <= 25) {
+				setShowScrollTop(false)
+			} else {
+				setShowScrollTop(true)
+			}
+		}
+	}, [setShowScrollBottom])
+
 	return (
 		<GridRightPanel>
+			{showScrollTop && <TopScrollShadow />}
 			<Label>
 				<IconWrapper>
 					<GrMoney size="14px" style={{ marginRight: '6px' }} />
@@ -53,11 +64,52 @@ export const RightPanel: FunctionComponent = () => {
 				Recent Donations
 			</Label>
 			<Content>
-				<Donations donations={donations} />
+				<DonationContent className="custom-scrollbar" onScroll={onScroll} ref={donationContentRef}>
+					<div ref={donationContentInnerRef}>
+						<Donations donations={donations ?? []} />
+					</div>
+				</DonationContent>
 			</Content>
+			{showScrollBottom && <BottomScrollShadow />}
 		</GridRightPanel>
 	)
 }
+
+const TopScrollShadow = styled.div`
+	content: '';
+	position: absolute;
+	top: 30px;
+	width: 100%;
+	height: 100px;
+	z-index: 100;
+	background: linear-gradient(#18181b, rgba(255, 255, 255, 0.001));
+`
+
+const BottomScrollShadow = styled.div`
+	content: '';
+	position: absolute;
+	bottom: 0;
+	width: 100%;
+	height: 100px;
+	z-index: 100;
+	background: linear-gradient(rgba(255, 255, 255, 0.001), #18181b);
+`
+
+const DonationContent = styled.div`
+	position: relative;
+	overflow-y: scroll;
+	height: 100%;
+	max-height: 300px;
+	padding-right: ${(p) => p.theme.space.s}px;
+
+	${(p) => p.theme.media.tablet} {
+		max-height: 400px;
+	}
+
+	${(p) => p.theme.media.desktop} {
+		max-height: unset;
+	}
+`
 
 const IconWrapper = styled.span`
 	display: flex;
@@ -70,4 +122,5 @@ const IconWrapper = styled.span`
 const GridRightPanel = styled.div`
 	background-color: ${(p) => p.theme.color.background};
 	grid-area: right-panel;
+	position: relative;
 `
