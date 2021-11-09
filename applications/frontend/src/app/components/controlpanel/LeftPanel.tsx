2747f556a1792da500b6424e41c828ee43b07cbf
Patrick Obermueller <patrick.obermueller@willhaben.at>
2021-11-09 21:16:05 +0100

    add size range select

--- applications/frontend/src/app/components/controlpanel/LeftPanel.tsx
+++ applications/frontend/src/app/components/controlpanel/LeftPanel.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react'
+import React, { useCallback, useState } from 'react'
 import { FunctionComponent } from 'react'
 import { Label, Content } from '../../../pages/controlpanel'
 import { styled } from '../../styles/Theme'
@@ -8,9 +8,12 @@ import { HiVolumeOff, HiVolumeUp } from 'react-icons/hi'
 import { FaPiggyBank } from 'react-icons/fa'
 import { CHARACTER_UPDATE, GlobalState, SETTINGS_UPDATE } from '@pftp/common'
 import { useSocket } from '../../hooks/useSocket'
+import { Range } from 'react-range'
+import { IoMdResize } from 'react-icons/io'
 
 export const LeftPanel: FunctionComponent<{ globalState: GlobalState }> = ({ globalState }) => {
 	const { socket } = useSocket()
+	const [scale, setScale] = useState([1])
 
 	const emitCharacterIsVisibleUpdate = useCallback(() => {
 		socket?.emit(CHARACTER_UPDATE, {
@@ -52,11 +55,81 @@ export const LeftPanel: FunctionComponent<{ globalState: GlobalState }> = ({ glo
 					>
 						<VolumeIndicator volume={globalState.settings.volume} />
 					</FatButton>
+
+					<FatButton style={{ cursor: 'default' }}>
+						<React.Fragment>
+							<Range
+								values={scale}
+								step={0.01}
+								min={0.25}
+								max={3}
+								onChange={(values) => setScale(values)}
+								renderTrack={({ props, children }) => (
+									<div
+										role="button"
+										tabIndex={-1}
+										/* eslint-disable react/prop-types */
+										onMouseDown={props.onMouseDown}
+										onTouchStart={props.onTouchStart}
+										style={{
+											...props.style,
+											height: '40px',
+											display: 'flex',
+											width: '100%',
+										}}
+									>
+										<div
+											ref={props.ref}
+											style={{
+												height: '5px',
+												width: '100%',
+												borderRadius: '2px',
+												alignSelf: 'center',
+												backgroundColor: 'rgba(255,255,255,0.2)',
+											}}
+										>
+											{children}
+										</div>
+									</div>
+								)}
+								renderThumb={({ props }) => (
+									<div
+										{...props}
+										style={{
+											/* eslint-disable react/prop-types */
+											...props.style,
+											height: '28px',
+											width: '28px',
+											borderRadius: '4px',
+											backgroundColor: '#049EE7',
+											display: 'flex',
+											justifyContent: 'center',
+											alignItems: 'center',
+										}}
+									>
+										<SizeIconWrapper>
+											<IoMdResize size={24} />
+										</SizeIconWrapper>
+									</div>
+								)}
+							/>
+						</React.Fragment>
+					</FatButton>
 				</ButtonsWrapper>
 			</Content>
 		</GridLeftPanel>
 	)
 }
+
+const SizeIconWrapper = styled.span`
+	display: flex;
+	align-items: center;
+	* {
+		padding: 2px;
+		stroke-width: 12px;
+		color: ${(p) => p.theme.color.white};
+	}
+`
 
 const VolumeIndicator: FunctionComponent<{ volume: number }> = ({ volume }) => {
 	const filledCirclesCount = getStepsFromVolume(volume)
