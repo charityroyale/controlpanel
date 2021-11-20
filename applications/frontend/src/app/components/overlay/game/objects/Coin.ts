3bb1713e45f672d3a3eac693636f0a168ba7762c
Patrick Obermueller <patrick.obermueller@willhaben.at>
2021-11-20 18:42:33 +0100

    scale up coin

--- applications/frontend/src/app/components/overlay/game/objects/Coin.ts
+++ applications/frontend/src/app/components/overlay/game/objects/Coin.ts
@@ -4,6 +4,7 @@ export class Coin extends Phaser.GameObjects.Sprite {
 	constructor(scene: Phaser.Scene, x: number, y: number, texture: string | Phaser.Textures.Texture) {
 		super(scene, x, y, texture)
 		this.name = 'coin'
+		this.setScale(2)
 
 		this.scene.tweens.add({
 			targets: this,
