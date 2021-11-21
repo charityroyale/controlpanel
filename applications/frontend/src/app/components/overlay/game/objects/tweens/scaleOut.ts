c5e67f4fe973f363ae559a185e280d31c87ca097
Patrick Obermueller <patrick.obermueller@willhaben.at>
2021-11-21 16:34:49 +0100

    increase alert shown to 6 seconds

--- applications/frontend/src/app/components/overlay/game/objects/tweens/scaleOut.ts
+++ applications/frontend/src/app/components/overlay/game/objects/tweens/scaleOut.ts
@@ -1,7 +1,7 @@
 import { GameObjects, Scene } from 'phaser'
 
 const scaleOutDuration = 500
-const scaleOutDelay = 5000
+const scaleOutDelay = 6000
 export const scaleOut = (scene: Scene, target: GameObjects.GameObject, onStart: () => void) => {
 	scene.tweens.add({
 		targets: target,
