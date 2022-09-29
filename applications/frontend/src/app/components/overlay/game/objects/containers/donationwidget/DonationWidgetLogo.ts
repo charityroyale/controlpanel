0429be0e097e5bd29b32724b8b98bd38c09989e0
Patrick Obermueller <patrick.obermueller@willhaben.at>
2022-09-29 08:40:08 +0200

    feature: add logo rotation to donationwidget

---
+++ applications/frontend/src/app/components/overlay/game/objects/containers/donationwidget/DonationWidgetLogo.ts
@@ -0,0 +1,30 @@
+import { DonationWidgetState } from '@cp/common'
+import { CHARITY_ROYALE_LOGO_IMAGE_KEY, MAKE_A_WISH_LOGO_IMAGE_KEY } from '../../../scenes/OverlayScene'
+
+export const donationWidgetLogoName = 'donationWidgetLogo'
+export class DonationWidgetLogo extends Phaser.GameObjects.Sprite {
+	constructor(scene: Phaser.Scene, x: number, y: number, state: DonationWidgetState) {
+		super(scene, x, y, CHARITY_ROYALE_LOGO_IMAGE_KEY)
+		this.name = donationWidgetLogoName
+		this.setOrigin(0, 0.5)
+		this.setScale(state.scale)
+		this.scene.add.existing(this)
+		this.startRotateTexture()
+	}
+
+	private startRotateTexture() {
+		this.scene.time.addEvent({
+			delay: 5000,
+			callback: () => this.swapTexture(),
+			loop: true,
+		})
+	}
+
+	private swapTexture() {
+		if (this.texture.key === CHARITY_ROYALE_LOGO_IMAGE_KEY) {
+			this.setTexture(MAKE_A_WISH_LOGO_IMAGE_KEY)
+		} else {
+			this.setTexture(CHARITY_ROYALE_LOGO_IMAGE_KEY)
+		}
+	}
+}
