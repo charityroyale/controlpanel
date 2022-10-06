ff6473a4798c1290fae07d00b3c0e64b1d610fd0
Patrick Obermueller <patrick.obermueller@willhaben.at>
2022-10-06 13:11:33 +0200

    feature: add second level pw

--- applications/frontend/src/pages/api/sessions.ts
+++ applications/frontend/src/pages/api/sessions.ts
@@ -1,7 +1,8 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import { UserSessionData, withSessionRoute } from '../../app/lib/session'
 
-const APPLICATION_PASSWORD = process.env.APPLICATION_PASSWORD
+const MAIN_APPLICATION_PASSWORD = process.env.MAIN_APPLICATION_PASSWORD
+const COMMUNITY_APPLICATION_PASSWORD = process.env.COMMUNITY_APPLICATION_PASSWORD
 export interface UserDTO {
 	username: string
 }
@@ -11,7 +12,10 @@ const sessionRoute = async (req: NextApiRequest, res: NextApiResponse) => {
 	if (req.method === 'POST') {
 		const { username, password } = req.body
 
-		if (typeof APPLICATION_PASSWORD === 'string' && password === APPLICATION_PASSWORD) {
+		if (
+			(typeof MAIN_APPLICATION_PASSWORD === 'string' && password === MAIN_APPLICATION_PASSWORD) ||
+			(typeof COMMUNITY_APPLICATION_PASSWORD === 'string' && password === COMMUNITY_APPLICATION_PASSWORD)
+		) {
 			const user: UserDTO = { username }
 			const userSession = req.session as UserSessionData
 			userSession.user = user
