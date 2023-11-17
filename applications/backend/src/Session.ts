8211323e8e44e08e4f7502557e6c3e3b8521231c
Patrick Obermueller <pat.obermueller@gmail.com>
2023-11-17 12:33:47 +0100

    fix min amount

--- applications/backend/src/Session.ts
+++ applications/backend/src/Session.ts
@@ -79,7 +79,10 @@ export default class Session {
 	}
 
 	public async triggerDonationAlert(donation: Donation) {
-		if (donation.message && donation.amount_net >= this.store.getState().settings.text2speech.minDonationAmount) {
+		if (
+			donation.message &&
+			(donation?.amount_net ?? donation.amount >= this.store.getState().settings.text2speech.minDonationAmount)
+		) {
 			await createTextToSpeechFile(
 				donation.message,
 				TTS_SPEAKER[this.store.getState().settings.text2speech.language],
