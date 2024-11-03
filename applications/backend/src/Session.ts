47368496c06b0e6f2e104d4fbd91bcc9820b9701
Patrick Obermueller <pat.obermueller@gmail.com>
2024-11-03 18:50:59 +0100

    fix safeguard

--- applications/backend/src/Session.ts
+++ applications/backend/src/Session.ts
@@ -119,7 +119,10 @@ export default class Session {
 		socket.on(REQUEST_MAW_INFO_JSON_DATA, () => {
 			this.store.dispatch(updateDonationWidget({ ...this.store.getState().donationWidget }))
 
-			if (Number(mawApiClient.mawInfoJsonData?.streamers[this.channel]?.current_donation_sum_net)) {
+			if (
+				mawApiClient.mawInfoJsonData?.streamers[this.channel]?.current_donation_sum_net &&
+				Number(mawApiClient.mawInfoJsonData?.streamers[this.channel]?.current_donation_sum_net)
+			) {
 				this.store.dispatch(
 					updateDonationGoal({
 						...this.store.getState().donationGoal,
