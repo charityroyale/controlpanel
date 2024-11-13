ebab035916232fec38a7f4db1ccf1e9ce2945095
Patrick Obermueller <pat.obermueller@gmail.com>
2024-11-13 10:29:34 +0100

    multistream a1esports channels

--- applications/backend/src/util/multiStreamUtil.ts
+++ applications/backend/src/util/multiStreamUtil.ts
@@ -5,6 +5,17 @@ const multiStreams = [
 	['icyvace', 'cherryylein'],
 	['xmrcr4zy', 's0ulrider'],
 	['stephantschany', 'yourhostlaura'],
+	[
+		'a1esports',
+		'a1esports-01',
+		'a1esports-02',
+		'a1esports-03',
+		'a1esports-04',
+		'a1esports-05',
+		'a1esports-06',
+		'a1esports-07',
+		'a1esports-08',
+	],
 ]
 
 const flatMultiStreams = multiStreams.reduce((prev, curr) => prev.concat(curr), [])
